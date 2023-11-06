e3da1bf493562c1a12331b278db29f0a2c7f75df
yupain <100sup4ik@gmail.com>
2023-11-06 23:38:49 +0300

    latestSolutions

--- bin/jenek_go.dart
+++ bin/jenek_go.dart
@@ -28,11 +28,10 @@ void task1() {
     'thirst' : 'thirst',
     'Second' : 'Second'
   };*/
-  int iterator = -1;
   // Iterator iterator = (massive as Iterator<dynamic>);
 
-  while (++iterator < (massive.length)) {
-    print(massive[iterator]);
+  for (var i in massive){
+    print(i);
   }
 }
 
@@ -50,7 +49,7 @@ void task2() {
   List massive = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   int iterator = massive.length;
 
-  while (--iterator >= 1) {
+  while (--iterator >= 0) {
     print(massive[iterator]);
   }
 }
@@ -66,12 +65,12 @@ void task2Jeka() {
 
 void task3() {
   // Выводит чётные элементы массива.
-  List massive = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  List array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   int iterator = -1;
 
-  while (++iterator < massive.length) {
-    if ((iterator + 1) % 2 == 0) {
-      print(massive[iterator]);
+  while (++iterator < array.length) {
+    if (array[iterator] % 2 == 0) {
+      print(array[iterator]);
     }
   }
 }
@@ -87,23 +86,21 @@ void task3Jeka() {
 
 void task4() {
   // Выводит все элементы массива через 1.
-  List massive = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  int iterator = -1;
+  List array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  while (++iterator < massive.length) {
-    if ((iterator + 1) % 2 == 1) {
-      print(massive[iterator]);
-    }
+  for (int i = 0; i < array.length; i++){
+    if (i % 2 == 1) print(array[i]);
   }
 }
 
 void task5() {
   // Выводит все элементы массива пока не встретится элемент -1.
-  List massive = [1, 2, 3, 4, 5, 6, -1, 7, 8, 9];
-  int iterator = -1;
-
-  while (++iterator < massive.length && massive[iterator] != -1) {
-    print(massive[iterator]);
-    //iterator++;
+  List array = [1, 2, 3, 4, 5, 6, -1, 7, 8, 9];
+  
+  for (final i in array){
+    if (i == -1)
+      break;
+    else
+      print(i);
   }
 }
