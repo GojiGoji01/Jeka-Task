43a104a08a1cc733ac2ddb418fab1a59e95c9714
yupain <100sup4ik@gmail.com>
2023-11-16 17:43:46 +0300

    futureUpdate

--- bin/future_learn.dart
+++ bin/future_learn.dart
@@ -1,10 +1,16 @@
+// import 'dart:js';
+
+import 'dart:collection';
+import 'dart:ffi';
+
 Future<void> main() async {
-  bool isHardLevel = false;
-  if (isHardLevel) {
-    _hardExample();
-  } else {
-    _explanation();
-  }
+  // bool isHardLevel = true;
+  // if (isHardLevel) {
+  //   _hardExample();
+  // } else {
+  //   _explanation();
+  // }
+  task1();
 }
 
 // <––––––––––––––––––––––––– FOUNDATION EXAMPLE –––––––––––––––––––––––––>
@@ -138,7 +144,60 @@ final class TestUser extends User {
 
 // <––––––––––––––––––––––––– KIRILL TASKS –––––––––––––––––––––––––>
 
-void task1() {
+final class StartRelationshipException implements Exception {
+  // не понимаю для чего тут надо использовать ключевое слово "final"
+  const StartRelationshipException(this.human1, this.human2);
+  // не понимаю для чего тут надо использовать ключевое слово "const"
+
+  final Human human1;
+  final Human human2;
+
+  @override
+  String toString() => '${human1.name} и ${human2.name} начали встречаться!';
+}
+
+final class AlreadyStartedRelationshipException implements Exception {
+  const AlreadyStartedRelationshipException(this.human);
+
+  final Human human;
+
+  @override
+  String toString() => 'К сожалению ${human.name} уже состоит в отношениях!';
+}
+
+final class SameSexException implements Exception {
+  const SameSexException(this.human1, this.human2);
+
+  final Human human1;
+  final Human human2;
+
+  @override
+  String toString() =>
+      '${human1.name} и ${human2.name} не могут состоять в отношениях, они одного пола!';
+}
+
+final class StopRelationship implements Exception {
+  const StopRelationship(this.human1, this.human2);
+
+  final Human human1;
+  final Human human2;
+
+  @override
+  String toString() => ('${human1.name} и ${human2.name} расстались!');
+}
+
+final class NotInRelationshipException implements Exception {
+  const NotInRelationshipException(this.human1, this.human2);
+
+  final Human human1;
+  final Human human2;
+
+  @override
+  String toString() =>
+      '${human1.name} и ${human2.name} не могут расстаться, они не состояли в отношениях!';
+}
+
+Future<void> task1() async {
   /*
   Скопируй свой код из classes.dart сюда и перепеши, заменив принты на throw, 
   пробрасывая какие-то экспешены и обрабатывая их. Сделай функции startRelationship
@@ -146,4 +205,91 @@ void task1() {
   друг с другом) Попробуй вызывать эти функции обоими способами (.then и await),
   и также используй соответсвующие обработчики ошибок для них.
   */
+  //На данном этапе это все как смог разобраться пока в future, пока что не
+  //нашел где искать инфу про ".then"
+  final kate = Female(name: 'Катя', age: 23);
+  final igor = Male(name: 'Игорь', age: 27);
+
+  igor.salutation(kate);
+  try {
+    await igor
+        .startRelationship(kate)
+        .then((value) => null)
+        .catchError((error) {
+      print(error);
+    });
+    igor.stopRelationship(kate);
+  } catch (e) {
+    print(e);
+  }
+  // try {
+  //   igor.stopRelationship(kate);
+  // } catch (e) {
+  //   print(e);
+  // }
+  try {
+    igor.stopRelationship(kate);
+  } catch (e) {
+    print(e);
+  }
+  igor.startRelationship(kate).then((value) => null).catchError((error) {
+    print(error);
+  });
 }
+
+abstract base class Human {
+  final String name;
+  final int age;
+  final Sex sex;
+  Human? relationshipStatus;
+
+  Human(this.name, this.age, this.sex);
+
+  void salutation(Human another) {
+    print('Привет, ${another.name}!');
+  }
+
+  Future<void> startRelationship(Human another) async {
+    final delay = await Future.delayed(Duration(seconds: 2), (() => null));
+    if (another.relationshipStatus == null &&
+        relationshipStatus == null &&
+        another.sex != sex) {
+      relationshipStatus = another;
+      another.relationshipStatus = this;
+      throw StartRelationshipException(this, another);
+    } else if (another.relationshipStatus != null) {
+      throw AlreadyStartedRelationshipException(another);
+    } else if (relationshipStatus != null) {
+      throw AlreadyStartedRelationshipException(this);
+    } else if (another.sex == sex) {
+      throw SameSexException(this, another);
+    }
+    return delay;
+  }
+
+  Future<void> stopRelationship(Human another) {
+    if (relationshipStatus == another) {
+      relationshipStatus = null;
+      another.relationshipStatus = null;
+      throw StopRelationship(this, another);
+    } else {
+      throw NotInRelationshipException(this, another);
+    }
+  }
+
+  @override
+  String toString() {
+    return '$name(Age: $age, Sex: ${sex.name})';
+  }
+}
+
+final class Male extends Human {
+  Male({required String name, required int age}) : super(name, age, Sex.male);
+}
+
+final class Female extends Human {
+  Female({required String name, required int age})
+      : super(name, age, Sex.female);
+}
+
+enum Sex { male, female }
