f81a17df127d695db220d58a8188c7be89b23d32
Sovetsk1y <39431565+Sovetsk1y@users.noreply.github.com>
2023-11-21 13:27:53 +0300

    future2 review1

--- bin/future_learn2.dart
+++ bin/future_learn2.dart
@@ -20,28 +20,27 @@ Future<void> main() async {
    */
 
   final service = RandomNameService();
-  final name = await service.loadName();
+  final names = await service.loadNames(amount: 10);
 
-  print(name);
+  print(names);
 }
 
 sealed class HttpException implements Exception {
   @override
   String toString() => switch (this) {
+        // норм
         RequestFailed() => 'Ошибка запроса. Проверьте параметры запроса!!',
-        BadRequest() =>
-          'Сервер обнаружил в запросе клиента синтаксическую ошибку!!',
-        ForbidenRequest() =>
+        Forbidden() =>
+          // Нет доступа
           'Пользователь ограничен в доступе к указанному ресурсу!!',
+        // Не найдено
         NotFound() => 'Ошибка в написании адреса Web-страницы!!',
       };
 }
 
-final class RequestFailed implements HttpException {}
+final class RequestFailed implements HttpException {} // 400 - ошибка запроса
 
-final class BadRequest implements HttpException {} //400 – ошибка запроса
-
-final class ForbidenRequest implements HttpException {} // 403 – доступ запрещен
+final class Forbidden implements HttpException {} // 403 – доступ запрещен
 
 final class NotFound implements HttpException {} //404 – не найдено
 
@@ -61,10 +60,11 @@ final class RandomNameService implements IRandomNameService {
         headers: {
           'x-api-key': 'f8f010f6e888495dbce7cc9c02c6cd65',
         });
+    // Вынеси это в функцию _handleError
     if (response.statusCode == 400) {
-      throw BadRequest();
+      throw RequestFailed();
     } else if (response.statusCode == 403) {
-      throw ForbidenRequest();
+      throw Forbidden();
     } else if (response.statusCode == 404) {
       throw NotFound();
     }
@@ -89,16 +89,18 @@ final class RandomNameService implements IRandomNameService {
           headers: {
             'x-api-key': 'f8f010f6e888495dbce7cc9c02c6cd65',
           });
+      // Вынеси это в функцию _handleError
       if (response.statusCode == 400) {
-        throw BadRequest();
+        throw RequestFailed();
       } else if (response.statusCode == 403) {
-        throw ForbidenRequest();
+        throw Forbidden();
       } else if (response.statusCode == 404) {
         throw NotFound();
       }
       if (response.statusCode != 200) {
         throw Exception('Something goes wrong..');
       }
+      // Не работает
       final listJson = jsonDecode(response.body) as List;
       list[i] = listJson[0];
     }
